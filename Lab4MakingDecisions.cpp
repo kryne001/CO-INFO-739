e44122e7f2b8cc41b662fa059a1904c12e10d658
Kyler Rynear <kylerrynear@kylers-mbp.lan>
2020-01-18 20:00:44 -0800

    Finished Lab4, corrected syntax errors, working fine. Need to add comments

--- Lab4MakingDecisions.cpp
+++ Lab4MakingDecisions.cpp
@@ -5,68 +5,68 @@
 #include <iostream>
 #include <cstdlib>
 #include <iomanip>
-const double LESS_THAN_10_THOU_SALES = 9,999;
-const double 50_THOU_SALES = 49,999;
-const double 50_THOU_SALES_COMMISSION_PCNT = 0.01;
-const double 100_THOU_SALES = 99,999;
-const double 100_THOU_SALES_COMMISSION_PCNT = 0.02;
-const double 200_THOU_SALES = 199,999;
-const double 200_THOU_SALES_COMMISSION_PCNT = 0.03;
-const double OVER_200_SALES = 200,000;
-const double OVER_200_SALES_COMMISSION_PCNT = 0.04;
-const double 250_THOU_SALES = 250,000;
-const double 250_THOU_SALES_BONUS = 500;
+const double LESS_THAN_TEN_THOU_SALES = 9999;
+const double FIFTY_THOU_SALES = 49999;
+const double FIFTY_THOU_SALES_COMMISSION_PCNT = 0.01;
+const double HUNDRED_THOU_SALES = 99999;
+const double HUNDRED_THOU_SALES_COMMISSION_PCNT = 0.02;
+const double TWOHUN_THOU_SALES = 199999;
+const double TWOHUN_THOU_SALES_COMMISSION_PCNT = 0.03;
+const double OVER_TWOHUN_SALES = 200000;
+const double OVER_TWOHUN_SALES_COMMISSION_PCNT = 0.04;
+const double TWOFIFTY_THOU_SALES = 250000;
+const double TWOFIFTY_THOU_SALES_BONUS = 500;
 using namespace std;
 
-double main()
+int main()
 {
     double salesAmount;
     double draw;
     double commission;
 
     cout << fixed << setprecision(2);
-    cout << "Enter Sales amount: ";
+    cout << "Enter Sales amount: $";
     cin >> salesAmount;
     cout << endl;
 
     if(salesAmount < 0)
     {
         cout << "ERROR: VALUE IS A NEGATIVE NUMBER" << endl
-             << "Please enter a value greater than zero: ";
+             << "Please enter a value greater than zero: $";
         cin >> salesAmount;
         cout << endl;
     }
 
-    cout << "Enter draw amount: ";
+    cout << "Enter draw amount: $";
     cin >> draw;
     cout << endl;
 
     if (draw < 0)
     {
         cout << "ERROR: VALUE IS A NEGATIVE NUMBER" << endl
-             << "Please enter a value greater than zero: ";
+             << "Please enter a value greater than zero: $";
         cin >> draw;
         cout << endl;
     }
 
-    if (salesAmount < LESS_THAN_10_THOU_SALES)
+    if (salesAmount < LESS_THAN_TEN_THOU_SALES)
     {
         cout << "Employee receives draw, to the amount of $" << draw << endl;
     }
-    else if (salesAmount > LESS_THAN_10_THOU_SALES)
+    else if (salesAmount > LESS_THAN_TEN_THOU_SALES)
     {
-        if(salesAmount > 50_THOU_SALES)
+        if(salesAmount > FIFTY_THOU_SALES)
         {
-            if(salesAmount > 100_THOU_SALES)
+            if(salesAmount > HUNDRED_THOU_SALES)
             {
-                if(salesAmount > 200_THOU_SALES)
+                if(salesAmount > TWOHUN_THOU_SALES)
                 {
-                    if(salesAmount >= 250_THOU_SALES)
+                    if(salesAmount >= TWOFIFTY_THOU_SALES)
                     {
                         commission = salesAmount
-                            * 200_THOU_SALES_COMMISSION_PCNT
-                            + 250_THOU_SALES_BONUS;
-                        cout << "$250,000 CLUB MEMBER!" << endl;
+                            * TWOHUN_THOU_SALES_COMMISSION_PCNT
+                            + TWOFIFTY_THOU_SALES_BONUS;
+                        cout << "$250,000 CLUB MEMBER!" << endl
                              << "Employee receives commission, "
                              << "to the amount of $"
                              << commission << endl;
@@ -74,21 +74,21 @@ double main()
                 }
                 else
                 {
-                    commission = salesAmount * 200_THOU_SALES_COMMISSION_PCNT;
+                    commission = salesAmount * TWOHUN_THOU_SALES_COMMISSION_PCNT;
                     cout << "Employee receives commission, to the amount of $"
                          << commission << endl;
                 }
             }
             else
             {
-                commission = salesAmount * 100_THOU_SALES_COMMISSION_PCNT;
+                commission = salesAmount * HUNDRED_THOU_SALES_COMMISSION_PCNT;
                 cout << "Employee receives commission, to the amount of $"
                      << commission << endl;
             }
         }
         else
         {
-            commission = salesAmount * 50_THOU_SALES_COMMISSION_PCNT;
+            commission = salesAmount * FIFTY_THOU_SALES_COMMISSION_PCNT;
             cout << "Employee receives commission, to the amount of $"
                  << commission << endl;
         }
