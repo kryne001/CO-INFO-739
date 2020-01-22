df337ab5258f3b8496957d0b1f0998c0aa3b0c6a
Kyler Rynear <kylerrynear@Kylers-MacBook-Pro.local>
2020-01-22 01:27:25 -0800

    Lab 5 full functionality, added comments, ready to submit

--- Lab5Loops.cpp
+++ Lab5Loops.cpp
@@ -12,12 +12,13 @@ const string LINE = "---------------------------------";
 
 int main()
 {
-    cout << fixed << setprecision(2);
+
+    cout << endl;
     cout << "The purpose of this program is to assist a Vineyard with their "
-         << "inventory. The user will provide the number of racks desired, "
-         << "the number of squares in each rack, and how many bottles in each "
-         << "rack. We will then calculate the number of total bottles, total "
-         << "cubes, number of empty cubes, and ratio of full to empty cubes."
+         << "inventory. The user will provide the number of racks in the cellar, "
+         << "the number of cubes in each rack, and how many bottles are in each "
+         << "rack. We will then provide the number of total bottles, total "
+         << "cubes, number of empty cubes, and percentage full of inventory."
          << endl << endl;
 
     int inputRacks;
@@ -25,83 +26,110 @@ int main()
     int numOfEmptyRacks = 0;
     double emptyRackCounter = 1;
     int squaresPerRack = 0;
-    int totalSquares = 0;
+    double totalSquares = 0;
     int bottlesPerRack = 0;
-    int totalBottles = 0;
+    double totalBottles = 0;
     int totalEmptyCubes = 0;
-    int inventoryPercentageFull = 0;
+    double inventoryPercentageFull = 0;
     vector<int> rackSizeCounter;
 
-
+    // initial user input to receive number of racks total
     cout << "Enter the number of racks in the wine cellar: ";
     cin >> inputRacks;
     cout << endl;
 
-    while (inputRacks < 0)
+    // check for value less than or equal to zero
+    while (inputRacks <= 0)
     {
-        cout << "Please enter a value of zero or greater: ";
+        cout << "Please enter a value greater than zero: ";
         cin >> inputRacks;
         cout << endl;
     }
 
-
+    // loop to find number of squares in each rack
     for (int i = 0; i < inputRacks; i++)
     {
-
+        // check if on fifth column, skip if true
         if((emptyRackCounter / EMPTY_RACK) != 1)
         {
             cout << "Enter number of squares in rack " << i+1 << ": ";
             cin >> squaresPerRack;
             cout << endl;
-            while (squaresPerRack < 0)
+            // check for zero or negative number
+            while (squaresPerRack <= 0)
             {
-                cout << "Please enter a value of zero or greater: ";
+                cout << "Please enter a value greater than zero: ";
                 cin >> squaresPerRack;
                 cout << endl;
             }
+            // add iteration amount to total amount
             totalSquares += squaresPerRack;
+            // log size of rack at iteration
             rackSizeCounter.push_back(squaresPerRack);
         }
         else
         {
+            // on fifth column, size is zero for logging purposes
             rackSizeCounter.push_back(0);
         }
+        // increment counter that checks for empty rack
         emptyRackCounter++;
     }
 
+    // reset counter for next user input
     emptyRackCounter = 1;
+    cout << LINE << endl;
 
+    // loop to find amount of bottles in each rack
     for (int i = 0; i < inputRacks; i++)
     {
+        // check if fifth rack, skip if true
         if((emptyRackCounter / EMPTY_RACK) != 1)
         {
             cout << "Enter number of bottles in rack " << i+1 << ": ";
             cin >> bottlesPerRack;
             cout << endl;
+
+            // check if value is negative number
             while (bottlesPerRack < 0)
             {
                 cout << "Please enter a value of zero or greater: ";
                 cin >> bottlesPerRack;
                 cout << endl;
             }
+
+            // check if number of bottles is greater than size of rack
             while (bottlesPerRack > rackSizeCounter.at(i))
             {
                 cout << "Please enter an amount for bottles that is less than "
-                     << "the size for the rack: ";
+                     << "the size of rack " << i+1 << " (size is "
+                     << rackSizeCounter.at(i) << "): ";
                 cin >> bottlesPerRack;
                 cout << endl;
             }
+
+            // add iteration number to total count
             totalBottles += bottlesPerRack;
         }
+
+        // increment rack counter to check for empty rack
         emptyRackCounter++;
     }
 
+    // find the remaining total of empty cubes
     totalEmptyCubes = totalSquares - totalBottles;
+
+    // find percentage full of inventory
     inventoryPercentageFull = (totalBottles/totalSquares) * TWO_DECIMAL_PLACES;
 
+    // print results
+    cout << fixed << setprecision(0);
     cout << "Total number of cubes: " << totalSquares << endl;
     cout << "Total number of bottles: " << totalBottles << endl;
     cout << "Number of empty cubes: " << totalEmptyCubes << endl;
+    cout << fixed << setprecision(2);
     cout << "The cellar is " << inventoryPercentageFull << "% full." << endl;
+
+    return 0;
 
 }
